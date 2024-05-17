3ef0418ee353fc77adcf37babc581caec11ce644
mustafaf8 <129125878+mustafaf8@users.noreply.github.com>
2024-05-17 16:51:03 +0300

    Yapay Sinir Ağı

---
+++ YSAP/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace YSAP
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEgit = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.listBoxSonuclar = new System.Windows.Forms.ListBox();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.btnYukle = new System.Windows.Forms.Button();
+            this.btnTestEt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEgit
+            // 
+            this.btnEgit.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEgit.Location = new System.Drawing.Point(334, 12);
+            this.btnEgit.Name = "btnEgit";
+            this.btnEgit.Size = new System.Drawing.Size(96, 48);
+            this.btnEgit.TabIndex = 0;
+            this.btnEgit.Text = "Eğit";
+            this.btnEgit.UseVisualStyleBackColor = true;
+            this.btnEgit.Click += new System.EventHandler(this.btnEgit_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 5;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.ForeColor = System.Drawing.Color.Black;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(35, 12);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(274, 266);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKaydet.Location = new System.Drawing.Point(334, 124);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(96, 49);
+            this.btnKaydet.TabIndex = 2;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // listBoxSonuclar
+            // 
+            this.listBoxSonuclar.Font = new System.Drawing.Font("Lucida Bright", 9F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxSonuclar.FormattingEnabled = true;
+            this.listBoxSonuclar.ItemHeight = 15;
+            this.listBoxSonuclar.Location = new System.Drawing.Point(461, 12);
+            this.listBoxSonuclar.Name = "listBoxSonuclar";
+            this.listBoxSonuclar.Size = new System.Drawing.Size(305, 259);
+            this.listBoxSonuclar.TabIndex = 3;
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTemizle.Location = new System.Drawing.Point(334, 229);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(96, 49);
+            this.btnTemizle.TabIndex = 4;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // btnYukle
+            // 
+            this.btnYukle.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnYukle.Location = new System.Drawing.Point(334, 179);
+            this.btnYukle.Name = "btnYukle";
+            this.btnYukle.Size = new System.Drawing.Size(96, 44);
+            this.btnYukle.TabIndex = 5;
+            this.btnYukle.Text = "Yükle";
+            this.btnYukle.UseVisualStyleBackColor = true;
+            this.btnYukle.Click += new System.EventHandler(this.btnYukle_Click);
+            // 
+            // btnTestEt
+            // 
+            this.btnTestEt.Font = new System.Drawing.Font("Yu Gothic UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTestEt.Location = new System.Drawing.Point(334, 66);
+            this.btnTestEt.Name = "btnTestEt";
+            this.btnTestEt.Size = new System.Drawing.Size(96, 52);
+            this.btnTestEt.TabIndex = 6;
+            this.btnTestEt.Text = "Test Et";
+            this.btnTestEt.UseVisualStyleBackColor = true;
+            this.btnTestEt.Click += new System.EventHandler(this.btnTestEt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 300);
+            this.Controls.Add(this.btnTestEt);
+            this.Controls.Add(this.btnYukle);
+            this.Controls.Add(this.listBoxSonuclar);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnEgit);
+            this.Controls.Add(this.btnTemizle);
+            this.Name = "Form1";
+            this.Text = "Yapay Sinir Ağı";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEgit;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.ListBox listBoxSonuclar;
+        private System.Windows.Forms.Button btnTemizle;
+        private System.Windows.Forms.Button btnYukle;
+        private System.Windows.Forms.Button btnTestEt;
+    }
+}
---
+++ YSAP/Form1.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace YSAP
+{
+    public partial class Form1 : Form
+    {
+        YapaySinirAgi YSA;
+
+        public Form1()
+        {
+            InitializeComponent();
+            YSA = new YapaySinirAgi();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            BaslangicDurumunaGetir();
+        }
+
+        private void BaslangicDurumunaGetir()
+        {
+            tableLayoutPanel1.Controls.Clear();
+            for (int i = 0; i < 7; i++) // Her satır için
+            {
+                for (int j = 0; j < 5; j++) // Her sütun için
+                {
+                    Panel panel = new Panel();
+                    panel.BackColor = System.Drawing.Color.White;
+                    panel.BorderStyle = BorderStyle.FixedSingle;
+                    panel.Click += Kare_Click;
+                    panel.Tag = 0;
+                    tableLayoutPanel1.Controls.Add(panel, j, i); // Paneli satır ve sütun konumuna göre ekleyin
+                }
+            }
+
+            listBoxSonuclar.Items.Clear(); // Liste temizleniyor
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            BaslangicDurumunaGetir();
+        }
+
+        private void btnEgit_Click(object sender, EventArgs e)
+        {
+            listBoxSonuclar.Items.Clear(); // Liste temizleniyor
+
+            double minHata = double.MaxValue;
+            char enIyiHarf = ' ';
+
+            foreach (char harf in new char[] { 'A', 'B', 'C', 'D', 'E' }) // Her harf için eğitim yapılıyor ve test ediliyor
+            {
+                double hata = YSA.Egit(harf);
+                listBoxSonuclar.Items.Add($"    {harf} => Eğitim Hatası: {hata}");
+
+                if (hata < minHata)
+                {
+                    minHata = hata;
+                    enIyiHarf = harf;
+                }
+            }
+
+            
+        }
+
+        private void btnTestEt_Click(object sender, EventArgs e)
+        {
+            listBoxSonuclar.Items.Clear(); // Liste temizleniyor
+
+            double[] giris = new double[35];
+
+            int index = 0;
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Panel)
+                {
+                    Panel panel = (Panel)control;
+                    giris[index++] = (int)panel.Tag;
+                }
+            }
+
+            char enIyiHarf = ' '; // En iyi harf başlangıçta belirtilmediği için
+                                  // Test işlemi için en iyi harfi tahmin etmek için en iyi harfi bulmamız gerekiyor
+            double minHata = double.MaxValue;
+            foreach (char harf in new char[] { 'A', 'B', 'C', 'D', 'E' })
+            {
+                double[] cikis = YSA.TahminEt(giris);
+
+                double hata = 0;
+                double[] egitim = null;
+                switch (harf)
+                {
+                    case 'A':
+                        egitim = new double[]{ 0,0,1,0,0, // A
+                                              0,1,0,1,0,
+                                              1,0,0,0,1,
+                                              1,0,0,0,1,
+                                              1,1,1,1,1,
+                                              1,0,0,0,1,
+                                              1,0,0,0,1};
+                        break;
+                    case 'B':
+                        egitim = new double[]{ 1,1,1,1,0, // B
+                                              1,0,0,0,1,
+                                              1,0,0,0,1,
+                                              1,1,1,1,0,
+                                              1,0,0,0,1,
+                                              1,0,0,0,1,
+                                              1,1,1,1,0};
+                        break;
+                    case 'C':
+                        egitim = new double[]{0,0,1,1,1, // C
+                                              0,1,0,0,0,
+                                              1,0,0,0,0,
+                                              1,0,0,0,0,
+                                              1,0,0,0,0,
+                                              0,1,0,0,0,
+                                              0,0,1,1,1};
+                        break;
+                    case 'D':
+                        egitim = new double[]{ 1,1,1,0,0, // D
+                                              1,0,0,1,0,
+                                              1,0,0,0,1,
+                                              1,0,0,0,1,
+                                              1,0,0,0,1,
+                                              1,0,0,1,0,
+                                              1,1,1,0,0};
+                        break;
+                    case 'E':
+                        egitim = new double[]{ 1,1,1,1,1, // E
+                                              1,0,0,0,0,
+                                              1,0,0,0,0,
+                                              1,1,1,1,1,
+                                              1,0,0,0,0,
+                                              1,0,0,0,0,
+                                              1,1,1,1,1};
+                        break;
+                    default:
+                        break;
+                }
+
+                for (int j = 0; j < 5; j++)
+                {
+                    hata += Math.Pow(egitim[j] - cikis[j], 2);
+                }
+
+                listBoxSonuclar.Items.Add($"    {harf} => Hata: {hata}");
+
+                if (hata < minHata)
+                {
+                    minHata = hata;
+                    enIyiHarf = harf;
+                }
+            }
+
+            listBoxSonuclar.Items.Add($"En İyi Harf: ({enIyiHarf}) , Hata: {minHata}");
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            YSA.AgirliklariKaydet();
+        }
+
+        private void btnYukle_Click(object sender, EventArgs e)
+        {
+            YSA.AgirliklariYukle();
+        }
+
+        private void Kare_Click(object sender, EventArgs e)
+        {
+            Panel kare = (Panel)sender;
+            if (kare.BackColor == System.Drawing.Color.Black)
+                kare.BackColor = System.Drawing.Color.White;
+            else
+                kare.BackColor = System.Drawing.Color.Black;
+
+            kare.Tag = kare.BackColor == System.Drawing.Color.Black ? 1 : 0;
+        }
+    }
+
+    public class YapaySinirAgi
+    {
+        double[,] agirlikGizli;
+        double[,] agirlikCikis;
+        double ogrenmeKatsayisi = 0.1;
+        int gizliNoronSayisi = 20;
+
+        public YapaySinirAgi()
+        {
+            agirlikGizli = new double[35, gizliNoronSayisi];
+            agirlikCikis = new double[gizliNoronSayisi, 5];
+
+            Random rastgele = new Random();
+            double minAgrlik = -0.1;
+            double maxAgrlik = 0.1;
+
+            for (int i = 0; i < 35; i++)
+            {
+                for (int j = 0; j < gizliNoronSayisi; j++)
+                {
+                    agirlikGizli[i, j] = minAgrlik + (maxAgrlik - minAgrlik) * rastgele.NextDouble();
+                }
+            }
+            for (int i = 0; i < gizliNoronSayisi; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    agirlikCikis[i, j] = minAgrlik + (maxAgrlik - minAgrlik) * rastgele.NextDouble();
+                }
+            }
+        }
+
+        public double Egit(char harf)
+        {
+            double[][] egitimVerisi = new double[][]
+            {
+                new double[]{ 0,0,1,0,0, // A
+                              0,1,0,1,0,
+                              1,0,0,0,1,
+                              1,0,0,0,1,
+                              1,1,1,1,1,
+                              1,0,0,0,1,
+                              1,0,0,0,1,},
+                new double[]{ 1,1,1,1,0, // B
+                              1,0,0,0,1,
+                              1,0,0,0,1,
+                              1,1,1,1,0,
+                              1,0,0,0,1,
+                              1,0,0,0,1,
+                              1,1,1,1,0,},
+                new double[]{ 0,0,1,1,1, // C
+                              0,1,0,0,0,
+                              1,0,0,0,0,
+                              1,0,0,0,0,
+                              1,0,0,0,0,
+                              0,1,0,0,0,
+                              0,0,1,1,1},
+                new double[]{ 1,1,1,0,0, // D
+                              1,0,0,1,0,
+                              1,0,0,0,1,
+                              1,0,0,0,1,
+                              1,0,0,0,1,
+                              1,0,0,1,0,
+                              1,1,1,0,0},
+                new double[]{ 1,1,1,1,1, // E
+                              1,0,0,0,0,
+                              1,0,0,0,0,
+                              1,1,1,1,1,
+                              1,0,0,0,0,
+                              1,0,0,0,0,
+                              1,1,1,1,1},
+            };
+
+            int harfIndex = harf - 'A'; // Harfin indeksi
+
+            double[] egitim = egitimVerisi[harfIndex];
+            double toplamHata = 0;
+
+            for (int iterasyon = 0; iterasyon < 5000; iterasyon++)
+            {
+                double[] giris = egitim;
+
+                double[] gizliCikis = new double[gizliNoronSayisi];
+                for (int j = 0; j < gizliNoronSayisi; j++)
+                {
+                    double net = 0;
+                    for (int k = 0; k < 35; k++)
+                    {
+                        net += giris[k] * agirlikGizli[k, j];
+                    }
+                    gizliCikis[j] = Sigmoid(net);
+                }
+
+                double[] cikis = new double[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    double net = 0;
+                    for (int k = 0; k < gizliNoronSayisi; k++)
+                    {
+                        net += gizliCikis[k] * agirlikCikis[k, j];
+                    }
+                    cikis[j] = Sigmoid(net);
+                }
+                //geri  yayılım (Backpropagation)
+
+                double[] geriHataCikis = new double[5];
+                for (int j = 0; j < 5; j++)
+                {
+                    geriHataCikis[j] = (egitim[j] - cikis[j]) * SigmoidTurevi(cikis[j]);
+                    toplamHata += Math.Pow(egitim[j] - cikis[j], 2);
+                }
+
+                double[] geriHataGizli = new double[gizliNoronSayisi];
+                for (int j = 0; j < gizliNoronSayisi; j++)
+                {
+                    double hata = 0;
+                    for (int k = 0; k < 5; k++)
+                    {
+                        hata += geriHataCikis[k] * agirlikCikis[j, k];
+                    }
+                    geriHataGizli[j] = hata * SigmoidTurevi(gizliCikis[j]);
+                }
+
+                for (int j = 0; j < 5; j++)
+                {
+                    for (int k = 0; k < gizliNoronSayisi; k++)
+                    {
+                        agirlikCikis[k, j] += ogrenmeKatsayisi * geriHataCikis[j] * gizliCikis[k];
+                    }
+                }
+
+                for (int j = 0; j < gizliNoronSayisi; j++)
+                {
+                    for (int k = 0; k < 35; k++)
+                    {
+                        agirlikGizli[k, j] += ogrenmeKatsayisi * geriHataGizli[j] * giris[k];
+                    }
+                }
+            }
+
+            return toplamHata;
+        }
+
+        public double[] TahminEt(double[] giris)
+        {
+            double[] gizliCikis = new double[gizliNoronSayisi];
+            for (int j = 0; j < gizliNoronSayisi; j++)
+            {
+                double net = 0;
+                for (int k = 0; k < 35; k++)
+                {
+                    net += giris[k] * agirlikGizli[k, j];
+                }
+                gizliCikis[j] = Sigmoid(net);
+            }
+
+            double[] cikis = new double[5];
+            for (int j = 0; j < 5; j++)
+            {
+                double net = 0;
+                for (int k = 0; k < gizliNoronSayisi; k++)
+                {
+                    net += gizliCikis[k] * agirlikCikis[k, j];
+                }
+                cikis[j] = Sigmoid(net);
+            }
+
+            return cikis;
+        }
+
+        private double Sigmoid(double net)
+        {
+            return 1 / (1 + Math.Exp(-net));
+        }
+
+        private double SigmoidTurevi(double cikis)
+        {
+            return cikis * (1 - cikis);
+        }
+
+        public void AgirliklariKaydet()
+        {
+            int gizliBoyut1 = agirlikGizli.GetLength(0);
+            int gizliBoyut2 = agirlikGizli.GetLength(1);
+            int cikisBoyut1 = agirlikCikis.GetLength(0);
+            int cikisBoyut2 = agirlikCikis.GetLength(1);
+
+            string agirlikGizliStr = $"{gizliBoyut1},{gizliBoyut2},{string.Join(",", agirlikGizli.Cast<double>())}";
+            string agirlikCikisStr = $"{cikisBoyut1},{cikisBoyut2},{string.Join(",", agirlikCikis.Cast<double>())}";
+
+            File.WriteAllText("agirlik_gizli.txt", agirlikGizliStr);
+            File.WriteAllText("agirlik_cikis.txt", agirlikCikisStr);
+            MessageBox.Show("Agırlıklar başarıyla kaydedildi.", "Kaydetme İşlemi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public void AgirliklariYukle()
+        {
+            if (File.Exists("agirlik_gizli.txt") && File.Exists("agirlik_cikis.txt"))
+            {
+                string[] agirlikGizliStr = File.ReadAllText("agirlik_gizli.txt").Split(',');
+                string[] agirlikCikisStr = File.ReadAllText("agirlik_cikis.txt").Split(',');
+
+                int gizliBoyut1 = int.Parse(agirlikGizliStr[0]);
+                int gizliBoyut2 = int.Parse(agirlikGizliStr[1]);
+                int cikisBoyut1 = int.Parse(agirlikCikisStr[0]);
+                int cikisBoyut2 = int.Parse(agirlikCikisStr[1]);
+
+                agirlikGizli = new double[gizliBoyut1, gizliBoyut2];
+                agirlikCikis = new double[cikisBoyut1, cikisBoyut2];
+
+                int index = 2;
+                for (int i = 0; i < gizliBoyut1; i++)
+                {
+                    for (int j = 0; j < gizliBoyut2; j++)
+                    {
+                        agirlikGizli[i, j] = Convert.ToDouble(agirlikGizliStr[index++]);
+                    }
+                }
+
+                index = 2;
+                for (int i = 0; i < cikisBoyut1; i++)
+                {
+                    for (int j = 0; j < cikisBoyut2; j++)
+                    {
+                        agirlikCikis[i, j] = Convert.ToDouble(agirlikCikisStr[index++]);
+                    }
+                }
+            }
+        }
+    }
+}
